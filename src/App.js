6bdfc3c7d9ed4f58b7625b9bf93cbd779250557e
LYT <limyt0909>
2021-07-16 17:46:30 +0900

    0716

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import LoginForm from "./Component/Login/LoginForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Auto0 test</header>
+      <header className="App-header">
+        Auto0 test
+        <LoginForm />
+      </header>
     </div>
   );
 }
