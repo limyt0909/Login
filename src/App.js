2c2f2efc64a271fa15f924c064a27da6ac6780e3
Lim Yong-tack <limyt0909@aonecustoms.com>
2021-07-28 14:05:38 +0900

    Login_API_add_0728

--- src/App.js
+++ src/App.js
@@ -1,15 +1,81 @@
-import "./App.css";
-import LoginForm from "./Component/Login/LoginForm";
+//https://shyunju7.tistory.com/37
 
-function App() {
+//https://blog.javabom.com/minhee/session/storage/localstorage-sessionstorage/react
+import axios from "axios";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+const LocalStorageComponent = () => {
+  const history = useHistory();
+  const [userName, setUserName] = useState("");
+  const [userpw, setUserpw] = useState("");
+  const [check, setCheck] = useState(false);
+
+  const saveData = () => {
+    const userObj = { name: userName };
+    console.log(userObj);
+    window.sessionStorage.setItem("userName", JSON.stringify(userObj));
+
+    const userObj2 = { pw: userpw };
+    console.log(userObj2);
+    window.sessionStorage.setItem("userpw", JSON.stringify(userObj2));
+  };
+
+  const callData = () => {
+    setCheck(true);
+  };
+
+  const onChange = (e) => {
+    setUserName(e.target.value);
+    setCheck(false);
+  };
+
+  const onChangepw = (e) => {
+    setUserpw(e.target.value);
+    setCheck(false);
+  };
+
+  //로그인 정보를 올리는 API
+  const login = () => {
+    const loginInfo = {
+      name: userName,
+      pw: userpw,
+    };
+    console.log(loginInfo);
+    axios.post("http://localhost:8080/login/courses", loginInfo).then((res) => {
+      history.push("/");
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        Auto0 test
-        <LoginForm />
-      </header>
+    <div>
+      <input
+        name="userName"
+        value={userName}
+        onChange={onChange}
+        placeholder="이름을 입력하세요!"
+      />
+      <input
+        name="pw"
+        value={userpw}
+        onChange={onChangepw}
+        placeholder="비밀번호 입력하세요!"
+      />
+
+      <button onClick={saveData}>저장하기</button>
+      <button onClick={callData}> 불러오기</button>
+      <button onClick="">로그아웃</button>
+      <button onClick={login}>로그인 API</button>
+
+      {check ? (
+        <p>
+          {window.sessionStorage.getItem("userName")}
+          {window.sessionStorage.getItem("userpw")}
+        </p>
+      ) : (
+        <> </>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default LocalStorageComponent;
--- src/Component/Login/LoginForm.js
+++ src/Component/Login/LoginForm.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import axios from "axios";
 
 //https://velog.io/@yaytomato/%ED%94%84%EB%A1%A0%ED%8A%B8%EC%97%90%EC%84%9C-%EC%95%88%EC%A0%84%ED%95%98%EA%B2%8C-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EC%B2%98%EB%A6%AC%ED%95%98%EA%B8%B0
+//https://darrengwon.tistory.com/563
+//https://im-developer.tistory.com/169
 
 const Container = styled.div`
   margin-top: 100px;
@@ -43,6 +45,7 @@ const Button = styled.div`
 `;
 //아디 비번 값 받기
 //값없으면 disabled
+
 function LoginForm() {
   const onLogin = (email, password) => {
     const data = {
@@ -75,6 +78,7 @@ function LoginForm() {
         type="password"
         placeholder="비밀번호를 입력해주세요"
       />
+
       <Button onClick={onLogin}>로그인</Button>
     </Container>
   );
